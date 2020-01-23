42053f5bb6eacc2365713ce36dd69eaa925aaada
KeerthanaKandaswamy <59925156+KeerthanaKandaswamy@users.noreply.github.com>
2020-01-23 16:11:56 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,51 @@
+var questions = [ 
+    {
+        "question": "Who is the inventor of WWW?",
+       "option1":  "Bill Gates",
+        "option2":  "Lee.N.Feyog",
+        "option3":  "N.Resscl",
+        "option4":  "Tim Berners lee",
+        "answer": "4"
+    },
+    
+    {
+    "question": "World Trade Organization (WTO) came into existence in ?",
+    
+    "option1":  "1992",
+    "option2":  "1993",
+    "option3":  "1994",
+    "option4":  "1995",
+   "answer": "4"
+    
+    },
+    
+    {
+        
+       "question":  "the best conductor of heat among the following is?",
+       "option1":  "Alcohol",
+    "option2":  "Mercury",
+    "option3":  "Ether",
+    "option4":  "Water",
+    "answer": "2",
+    },
+    
+    {
+    "question": "SAARC was formed in?",
+    "option1": "1982",
+    "option2":  "1984",
+    "option3":  "1985",
+    "option4":  "1986",
+    "answer": '3'
+    },
+    
+    {
+    "question": "National Science Day is observed on?",
+    "option1": "January 4",
+    "option2":  "February 28",
+    "option3":  "March 11",
+    "option4":  "August 5",
+    "answer": "2",
+    }
+    
+    ];
+    
